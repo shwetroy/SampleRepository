6c0f98ca83f762e207a831c1e5c4edeb9747c74a
USER <USER@LAPTOP-FTOETBD1>
2022-06-16 17:14:40 +0530

    Commit Test

---
+++ src/test/java/Repository/SampleTest1.java
@@ -0,0 +1,12 @@
+package Repository;
+
+public class SampleTest1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Test  Started");
+		System.out.println("Test Stopped");
+
+	}
+
+}
---
+++ src/test/java/Repository/Test1.java
@@ -0,0 +1,12 @@
+package Repository;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+		
+System.out.println("jenkins Started");
+System.out.println("jenkins stopped");
+	}
+
+}
